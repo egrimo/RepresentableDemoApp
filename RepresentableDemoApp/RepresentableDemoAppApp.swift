367250eb4d6768c0de88bead77e0f772f559cb1a
Ege Sucu <egesucu@icloud.com>
2022-02-12 15:47:36 +0300

    Initial Commit

---
+++ RepresentableDemoApp/RepresentableDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepresentableDemoAppApp.swift
+//  RepresentableDemoApp
+//
+//  Created by Ege Sucu on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RepresentableDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
