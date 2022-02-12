a3bea3e070a493d79eac65036c0d3dbacb7785e4
Ege Sucu <egesucu@icloud.com>
2022-02-12 16:01:39 +0300

    initial design implemented

--- RepresentableDemoApp/ContentView.swift
+++ RepresentableDemoApp/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack{
+                Image("default-image")
+                    .resizable()
+                    .frame(width: 250, height: 250, alignment: .center)
+                    .scaledToFill()
+                    .onTapGesture {
+                        //Open Image Picker
+                    }
+                    .cornerRadius(20)
+                    .shadow(radius: 8)
+                    .padding(20)
+                    .padding(.bottom)
+                Text("Select your image by tapping the image above.")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        //Reset Image
+                    } label: {
+                        Text("Reset")
+                            .foregroundColor(.red)
+                    }
+                }
+        }
+            .navigationTitle(Text("Image Picker Example"))
+        }
     }
 }
 
